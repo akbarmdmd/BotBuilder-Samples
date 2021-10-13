c918f86db08031dfabcc8c0eb3ea39b9d727f351
Md Akbar <mdakbar@microsoft.com>
2021-10-13 18:56:54 +0530

    GetMessages api

--- ScheduleMeetingExtension/Bots/TeamsMessagingExtensionsActionBot.cs
+++ ScheduleMeetingExtension/Bots/TeamsMessagingExtensionsActionBot.cs
@@ -16,6 +16,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Microsoft.BotBuilderSamples.Helpers;
 using Microsoft.BotBuilderSamples.Models;
+using System.Net.Http;
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
@@ -33,7 +34,6 @@ namespace Microsoft.BotBuilderSamples.Bots
             switch (action.CommandId)
             {
                 case "scheduleMeeting":
-                    //TODO: call create meeting api and return adaptive card in message.
                     return ScheduleMeetingResponse(turnContext, action);
             }
             return new MessagingExtensionActionResponse();
@@ -45,6 +45,8 @@ namespace Microsoft.BotBuilderSamples.Bots
             var a = action.Data.ToString();
             CustomFormResponse data = JsonConvert.DeserializeObject<CustomFormResponse>(a);
 
+            //TODO: call create meeting api and on success return adaptive card in message.
+
             var attachments = new List<MessagingExtensionAttachment>();
             attachments.Add(new MessagingExtensionAttachment
             {
@@ -53,9 +55,10 @@ namespace Microsoft.BotBuilderSamples.Bots
                 {
                     Body = new List<AdaptiveElement>()
                     {
-                      new AdaptiveTextInput() { Id = "title", Value = data.Title, Label = "Title" },
-                      new AdaptiveTextInput() { Id = "attendees", Value = string.Join("; ", data.Attendees), Label = "Attendees" },
-                      new AdaptiveTextInput() { Id = "time", Value = data.Time, Label = "Time" },
+                      new AdaptiveTextBlock() { Text = $"Meeting created with below details - ", Color = AdaptiveTextColor.Accent },
+                      new AdaptiveTextBlock() { Text = $"**Title** : {data.Title}" },
+                      new AdaptiveTextBlock() { Text = $"**Attendees** : {string.Join("; ", data.Attendees)}" },
+                      new AdaptiveTextBlock() { Text = $"**Time** : {data.Time}" },
                     },
                 },
             });
@@ -91,7 +94,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             switch (action.CommandId)
             {
                 case "scheduleMeeting":
-                    return MeetingDetails(turnContext, action);
+                    return await MeetingDetails(turnContext, action);
                 default:
                     // we are handling two cases within try/catch block 
                     //if the bot is installed it will create adaptive card attachment and show card with input fields
@@ -139,9 +142,31 @@ namespace Microsoft.BotBuilderSamples.Bots
             }
         }
 
-        private MessagingExtensionActionResponse MeetingDetails(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action)
+        private async Task<MessagingExtensionActionResponse> MeetingDetails(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action)
         {
-            // @esha fetch required properties from context or action, then call graph apis to fetch converstation details like conversation title, conversation attendees
+            // TODO Fetch required properties from context or action, then call graph apis to fetch converstation details like
+            // conversation title, conversation attendees, time (using find meeting times or LU on message)
+            var groupId = "b77dde63-1f70-4668-95d5-0c34e038514b";
+            var tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
+            var threadId = "19:dbad371212c0450f8c63da35ef7a0484@thread.tacv2";
+            var conversationId = "19:dbad371212c0450f8c63da35ef7a0484@thread.tacv2;messageid=1634033527688";
+
+            var token = "eyJ0eXAiOiJKV1QiLCJub25jZSI6IlJxc1dwRTkwN2gwZTBxejBEdHBSZG9hY3AyUVlocUtDWVFSNUdoNTRwZWMiLCJhbGciOiJSUzI1NiIsIng1dCI6Imwzc1EtNTBjQ0g0eEJWWkxIVEd3blNSNzY4MCIsImtpZCI6Imwzc1EtNTBjQ0g0eEJWWkxIVEd3blNSNzY4MCJ9.eyJhdWQiOiJodHRwczovL3N1YnN0cmF0ZS5vZmZpY2UuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3LyIsImlhdCI6MTYzNDEyMzI4MiwibmJmIjoxNjM0MTIzMjgyLCJleHAiOjE2MzQxMjcxODIsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVVFBdS84VEFBQUE4NnlBV3JhMGFwd1FQUGd6TzlUT1JmY0dJYktOTUhRUUxXcGozaC9qdmRNUFMvcG5KcExNQmhQZ01COHQ1dUxJR0dtSk95ei9zc01VN056YVRxQjRsQT09IiwiYW1yIjpbInJzYSIsIm1mYSJdLCJhcHBpZCI6ImQzNTkwZWQ2LTUyYjMtNDEwMi1hZWZmLWFhZDIyOTJhYjAxYyIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiMjE5OTVlNWYtNDMzOS00M2QyLTg0YTItMjY5Yzg5ZDY3MmVjIiwiZmFtaWx5X25hbWUiOiJBa2JhciIsImdpdmVuX25hbWUiOiJNZCIsImlwYWRkciI6IjI3LjYwLjE0MC45NyIsIm5hbWUiOiJNZCBBa2JhciIsIm9pZCI6ImI4ZGExOTBhLTEwMDYtNDdiYy1iOTZjLWYwZjE0ZDE3ZWQ1NyIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0yMTQ2NzczMDg1LTkwMzM2MzI4NS03MTkzNDQ3MDctMjI0MDY4NSIsInB1aWQiOiIxMDAzM0ZGRkEyMEJEMkQwIiwicmgiOiIwLkFSb0F2NGo1Y3ZHR3IwR1JxeTE4MEJIYlI5WU9XZE96VWdKQnJ2LXEwaWtxc0J3YUFFOC4iLCJzY3AiOiJBY3Rpdml0eUZlZWQtSW50ZXJuYWwuUmVhZFdyaXRlIENhbGVuZGFycy5SZWFkV3JpdGUgQ29sbGFiLUludGVybmFsLlJlYWQgQ29udGFjdHMuUmVhZFdyaXRlIENvcmVJdGVtLUludGVybmFsLlJlYWQgRVdTLkFjY2Vzc0FzVXNlci5BbGwgRmlsZXMuUmVhZCBGaWxlcy5SZWFkV3JpdGUuQWxsIEdyb3VwLlJlYWRXcml0ZS5BbGwgTWFpbC5SZWFkLkFsbCBNYWlsLlJlYWRXcml0ZSBNYWlsLlNlbmQgTWFwaUh0dHAuQWNjZXNzQXNVc2VyLkFsbCBOb3Rlcy5SZWFkV3JpdGUgTm90ZXMtSW50ZXJuYWwuUmVhZFdyaXRlIE9mZmljZUZlZWQtSW50ZXJuYWwuUmVhZFdyaXRlIE9mZmljZUludGVsbGlnZW5jZS1JbnRlcm5hbC5SZWFkV3JpdGUgUGVvcGxlUHJlZGljdGlvbnMtSW50ZXJuYWwuUmVhZCBQcml2aWxlZ2UuRUxUIFJvYW1pbmdVc2VyU2V0dGluZ3MuUmVhZFdyaXRlIFNpZ25hbHMuUmVhZCBTaWduYWxzLlJlYWRXcml0ZSBTdWJzdHJhdGVTZWFyY2gtSW50ZXJuYWwuUmVhZFdyaXRlIFRhZ3MuUmVhZFdyaXRlIFRhc2tzLlJlYWRXcml0ZSBUb2RvLUludGVybmFsLlJlYWRXcml0ZSB1c2VyX2ltcGVyc29uYXRpb24gVXNlci1JbnRlcm5hbC5SZWFkIiwic3ViIjoibmlQSVk2NXU5eHZNZ3p3OTBJX3d1Y3BHRDk0QU95cGJ0TVVvYkkxcDhvZyIsInRpZCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsInVuaXF1ZV9uYW1lIjoibWRha2JhckBtaWNyb3NvZnQuY29tIiwidXBuIjoibWRha2JhckBtaWNyb3NvZnQuY29tIiwidXRpIjoiLUkydzBmakhCMGlQZlhSNkxtaGxBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.h7Guf2nuZC7vYLxJwQKw9unYS0dHPi6b8OX3PzoooJ_KkliXYuHZ2IYel3abEg7kIvBT82O59X75ciISdZRUgBbwmgP6ldp_bAuX7sN3DeWXVlcw_2WuxPj9M2eX3F1pNcWLvFjP7MxeqG2nPrtlKEoIkiIxo5wu9AEL8GY1wMWbdumGLTIhkk13GojrOESDOZV4yFJjqtIzUEIC8-hhm40nmWKC55be4yWQplwk5Ri93DfxOg_ze2ne1aB6NrHgDuwhjfLbapTDAaTfMD9AwHD9BELs_Y-6GURCZfWr4wi8bgaf2oNS3cWletYpiZdBe7YM4RoTRz0GeWx2v8xtXA";
+            var preferHeader = "exchange.behavior='SubstrateDefaultFolders, OpenComplexTypeExtensions, ApplicationDataIP'";
+            
+            var url = "https://substrate.office.com/api/beta/groups/" + groupId + "@" + tenantId + 
+                string.Format("/DefaultFolders/TeamsMessagesData/Messages?$filter=ClientThreadId eq '{0}' and ClientConversationId eq '{1}'", threadId, conversationId);
+            
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            client.DefaultRequestHeaders.Add("Prefer", preferHeader);
+
+            var result = await client.GetAsync(url);
+
+            var customerJsonString = await result.Content.ReadAsStringAsync();
+
+            var deserialized = JsonConvert.DeserializeObject<TeamsMessagesData>(custome‌​rJsonString);
+
             var response = new MessagingExtensionActionResponse()
             {
                 Task = new TaskModuleContinueResponse()
--- ScheduleMeetingExtension/Model/CustomForm.cs
+++ ScheduleMeetingExtension/Model/CustomForm.cs
@@ -1,9 +1,35 @@
-﻿namespace Microsoft.BotBuilderSamples.Models
+﻿using System;
+
+namespace Microsoft.BotBuilderSamples.Models
 {
     public class CustomFormResponse
     {
         public string Title { get; set; }
         public string Attendees { get; set; }
         public string Time { get; set; }
+    }
+
+    public class TeamsMessagesData
+    {
+        public Message[] Value { get; set; }
+    }
+
+    public class Message
+    {
+        public string InternetMessageId { get; set; }
+        public string Subject { get; set; }
+        public string BodyPreview { get; set; }
+        public From Sender { get; set; }
+    }
+
+    public class From
+    {
+        public EmailAddress EmailAddress { get; set; }
+    }
+
+    public class EmailAddress
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
